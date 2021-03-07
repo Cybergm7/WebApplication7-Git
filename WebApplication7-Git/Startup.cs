e12690c15d1534a596198cc30a8a5c53b01501ae
reg sapetrainer <reg.sapetrainer@outlook.com>
2021-03-06 19:02:12 -0600

    Modified-first-hello

--- WebApplication7-Git/Startup.cs
+++ WebApplication7-Git/Startup.cs
@@ -32,7 +32,7 @@ namespace WebApplication7_Git
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World  First Change");
                 });
             });
         }
